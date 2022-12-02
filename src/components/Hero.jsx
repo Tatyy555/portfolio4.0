a0032a277242a34457618774d9a0ce1a6b0809bc
Tatsuya Yamaguchi <tatsuya852yamaguchi@gmail.com>
2022-12-02 10:13:02 +0900

    fix:title image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,27 +11,26 @@ function Hero() {
   }, []);
   const [text] = useTypewriter({
     words: [
-      "Nice to meet you!",
       "Thank you for visiting.",
       "I'm Tatsuya Yamaguchi.",
-      "Guy-who-loves-Manga.tsx",
-      "<ButLoves-CodeMore />",
+      "Guy-who-loves-Manga.js",
+      "<ButLoves-To-CodeMore />",
     ],
     loop: true,
     delaySpeed: 2000,
   });
   return (
-    <div className="font-Poppins flex flex-col w-screen h-screen text-center bg-gray-900">
-      <h1 className=" mt-36 md:mt-48 text-5xl sm:text-6xl md:text-7xl xl:text-8xl 2xl:text-9xl text-white leading-relaxed lg:pb-8 lg:leading-loose z-10">
-        <span className="mr-3 overflow-hidden text-ellipsis underline decoration-white decoration-[0.5rem]">
+    <div className="font-Pacifico flex flex-col w-screen h-screen text-center bg-gray-900">
+      <h1 className=" mt-40 md:mt-48 text-5xl sm:text-6xl md:text-7xl xl:text-8xl 2xl:text-9xl text-slate-100 leading-relaxed lg:pb-8 lg:leading-loose z-10">
+        <span className="mr-3 overflow-hidden text-ellipsis">
           {text}
         </span>
         <Cursor cursorColor="#FFFFFF" />
       </h1>
-      <p className="text-white text-3xl italic lg:text-5xl font-bold font-Arial mt-2  z-10">
+      <p className="text-slate-400 text-3xl italic lg:text-5xl font-bold font-Arial mt-16  z-10">
         a web developer
       </p>
-      <canvas className="h-[70%] sm:h-[80%] w-screen absolute top-0 fluid-image"></canvas>
+      <canvas className="h-[80%] w-screen absolute top-0 fluid-image"></canvas>
     </div>
   );
 }
