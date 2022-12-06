83d11ba0899144dda27e73ee0fe795fd579bb2c2
Tatsuya Yamaguchi <tatsuya852yamaguchi@gmail.com>
2022-12-06 16:37:47 +0900

    fix: adjust a little hero image

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -22,12 +22,12 @@ function Hero() {
   return (
     <div className="flex flex-col w-screen h-[80%] text-center snap-center relative">
       <canvas className=" w-screen  h-full absolute fluid-image" />
-      <div className=" flex flex-col w-screen ">
-        <h1 className="font-Pacifico  mt-40 md:mt-48 text-5xl sm:text-6xl md:text-7xl xl:text-8xl 2xl:text-9xl text-slate-100 leading-relaxed lg:pb-8 lg:leading-loose z-10">
+      <div className=" flex flex-col w-screen items-center">
+        <h1 className="font-Pacifico  mt-40 md:mt-48 text-6xl sm:text-6xl md:text-7xl xl:text-8xl 2xl:text-9xl text-slate-100 leading-relaxed lg:pb-8 lg:leading-loose z-10">
           <span className="mr-3 overflow-hidden text-ellipsis">{text}</span>
           <Cursor cursorColor="#FFFFFF" />
         </h1>
-        <p className="text-slate-400 text-3xl lg:text-5xl font-bold font-Arial mt-16  z-10">
+        <p className="text-slate-400 absolute text-3xl lg:text-5xl font-bold font-Arial bottom-[10rem] sm:bottom-[16rem]  z-10">
           a web developer
         </p>
       </div>
