1e572b6f4e70af1ca8667fd3ec00fe8ad2b4beb7
Tatsuya Yamaguchi <tatsuya852yamaguchi@gmail.com>
2022-12-06 14:07:24 +0900

    change the protfolio icon

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,14 +20,12 @@ function Hero() {
     delaySpeed: 2000,
   });
   return (
-    <div className="font-Pacifico flex flex-col w-screen h-screen text-center bg-gray-900">
-      <h1 className=" mt-40 md:mt-48 text-5xl sm:text-6xl md:text-7xl xl:text-8xl 2xl:text-9xl text-slate-100 leading-relaxed lg:pb-8 lg:leading-loose z-10">
-        <span className="mr-3 overflow-hidden text-ellipsis">
-          {text}
-        </span>
+    <div className="flex flex-col w-screen h-screen text-center bg-gray-900">
+      <h1 className="font-Pacifico  mt-40 md:mt-48 text-5xl sm:text-6xl md:text-7xl xl:text-8xl 2xl:text-9xl text-slate-100 leading-relaxed lg:pb-8 lg:leading-loose z-10">
+        <span className="mr-3 overflow-hidden text-ellipsis">{text}</span>
         <Cursor cursorColor="#FFFFFF" />
       </h1>
-      <p className="text-slate-400 text-3xl italic lg:text-5xl font-bold font-Arial mt-16  z-10">
+      <p className="text-slate-400 text-3xl lg:text-5xl font-bold font-Arial mt-16  z-10">
         a web developer
       </p>
       <canvas className="h-[80%] w-screen absolute top-0 fluid-image"></canvas>
