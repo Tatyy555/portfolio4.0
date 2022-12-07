760818933b3662a51b598b8b2b5706fab0efc1ad
Tatsuya Yamaguchi <tatsuya852yamaguchi@gmail.com>
2022-12-07 16:49:52 +0900

    fix: adjust for iphone

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -10,28 +10,28 @@ function ProjectSection() {
     {
       _id: 1,
       link: "",
-      image: <HiOutlineLibrary className="text-white h-60 w-60" />,
+      image: <HiOutlineLibrary className="text-white h-24 w-24 sm:h-60 sm:w-60" />,
       title: "Personal Library",
       summary: "Personal Products and Certificates with Timeline",
     },
     {
       _id: 2,
       link: "https://github.com/Tatyy555",
-      image: <AiFillGithub className="text-white h-60 w-60" />,
+      image: <AiFillGithub className="text-white h-24 w-24 sm:h-60 sm:w-60" />,
       title: "Github",
       summary: "Name: Tatyy555",
     },
     {
       _id: 3,
       link: "https://twitter.com/4k7hB",
-      image: <AiOutlineTwitter className="text-white h-60 w-60" />,
+      image: <AiOutlineTwitter className="text-white h-24 w-24 sm:h-60 sm:w-60" />,
       title: "Twitter",
       summary: "Name: TATYY",
     },
     {
       _id: 4,
       link: "https://www.kaggle.com/tatyy555",
-      image: <FaKaggle className="text-white h-60 w-60" />,
+      image: <FaKaggle className="text-white h-24 w-24 sm:h-60 sm:w-60" />,
       title: "Kaggle",
       summary: "Name: TATYY555",
     },
@@ -60,7 +60,7 @@ function ProjectSection() {
         About Myself
       </h1>
       <div className="bg-gray-800 sm:p-10 ">
-        <div className="grid sm:grid-cols-2 lg:grid-cols-4">
+        <div className="grid grid-cols-2 lg:grid-cols-4">
           {projects.map((project, i) => (
             <a
               key={project?._id}
@@ -68,7 +68,7 @@ function ProjectSection() {
               rel="noreferrer"
               target="_blank"
             >
-              <div className="flex flex-col cursor-pointer items-center transition-all duration-200 hover:scale-105 p-5 bg-my-skyblue/20 m-5 rounded-lg">
+              <div className="flex flex-col cursor-pointer items-center transition-all duration-200 hover:scale-105 p-5 bg-my-skyblue/20 sm:m-5 rounded-lg">
                 <motion.div
                   initial={{ y: -30, opacity: 0 }}
                   transition={{ duration: 1 }}
@@ -79,8 +79,8 @@ function ProjectSection() {
                   {project?.image}
                 </motion.div>
                 <div className="pt">
-                  <h2 className="text-3xl text-center">{project?.title}</h2>
-                  <p className="mt-2">{project?.summary}</p>
+                  <h2 className="text-xl sm:text-3xl text-center">{project?.title}</h2>
+                  <p className="text-[0.6rem] sm:text-base mt-2">{project?.summary}</p>
                 </div>
               </div>
             </a>
