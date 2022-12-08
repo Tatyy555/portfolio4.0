b5179e85d9653986ffb951147406324dcdd1d4fd
Tatsuya Yamaguchi <tatsuya852yamaguchi@gmail.com>
2022-12-08 11:48:57 +0900

    fix: font type of footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="flex h-[6vh] justify-around items-center lg:text-xl border-t">
+    <footer className="flex h-[6vh] justify-around items-center lg:text-xl border-t font-Montserrat">
       <p>Designed and Created by Yamaguchi</p>
     </footer>
   );
